918abd38ebbfb066fe7f44785c112707e16ef7d7
Sandhiya Ganesan <sandhiya.ganesan@aspiresys.com>
2024-02-17 10:06:01 +0530

    datas

--- Angular/Angular/src/app/services/app.service.ts
+++ Angular/Angular/src/app/services/app.service.ts
@@ -2,16 +2,68 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { App } from '../interface/app';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  private userPayload:any;
+
   public selectedApp: App = {} as App;
   apiBaseAddress = environment.apiBaseAddress;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private route:Router) { }
 
   getApp() {
     return this.http.get<App[]>(this.apiBaseAddress + 'api/App');
   }
+  register(model:any){
+    return this.http.post<any>(this.apiBaseAddress +'api/Auth/register',model);
 }
+login(user:any){
+  return this.http.post<any>(this.apiBaseAddress +'api/Auth',user).pipe(
+    map((response:any)=>{
+      const user= response;
+      if(user){
+        localStorage.setItem('token',JSON.stringify(user));
+        
+      }
+    })
+  )
+}
+
+
+ 
+storeToken(tokenValue:any){
+  localStorage.setItem('token',tokenValue)
+ }
+ getToken(){
+  return localStorage.getItem('token')
+ }
+ isLoggedIn():boolean{
+  return !!localStorage.getItem('token')
+ }
+ signOut()
+ {
+  localStorage.clear();
+  this.route.navigate(['login'])
+ }
+
+ 
+ getFullNameFromToken(){
+  if(this.userPayload)
+  {
+    return this.userPayload.email;
+
+  }
+
+ }
+ getRoleFromToken(){
+  if(this.userPayload)
+  {
+    return this.userPayload.role;
+  }
+ }
+}
+
